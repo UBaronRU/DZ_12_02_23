7189fb749f46ced0bd4a0c445fce16ce219b79ff
UBaronRU <ubaron@mail.ru>
2023-02-12 22:21:10 +0300

    Primer02 is down

---
+++ Primer02/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+## Primer02
+Найти сумму элементов от M до N, N и M заданы
+*/
+int N = new Random().Next(1,10); 
+int M = new Random().Next(1,10); 
+int summElements =0;
+Console.WriteLine();
+Console.Write($"Находим сумму элементов от {M} до {N}"); 
+Console.WriteLine();
+if (M > N)
+{
+   for (int i = M; i >= N; i--)
+    {   
+    summElements=summElements+i;
+    }     
+}
+else
+{
+   for (int i = M; i <= N; i++)
+{
+    summElements=summElements+i;
+} 
+}
+Console.Write($" Сумма элементов от {M} до {N} равнв {summElements}"); 
