00d0f5acef64a54efd98b728e6c9a1b1de29ed6f
UBaronRU <ubaron@mail.ru>
2023-02-12 23:50:40 +0300

    Primer05 is down

---
+++ Primer05/Program.cs
@@ -0,0 +1,14 @@
+﻿/*
+## Primer05
+Написать программу возведения числа А в целую стень B
+*/
+int M = new Random().Next(1,6); 
+int N = new Random().Next(1,6); 
+Console.WriteLine($"Число {M} в степени {N} = {stepen(M, N)}");
+
+int stepen (int M, int N)
+{
+    if (N == 1) return M;
+    if (N > 1) return M*stepen(M, N-1);
+    return stepen(M, N);
+}
