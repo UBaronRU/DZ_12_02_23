ad26ea10c721661c00475e213fcd419385054230
UBaronRU <ubaron@mail.ru>
2023-02-12 23:20:51 +0300

    Primer04 is down

---
+++ Primer04/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+## Primer04
+Написать программу вычисления функции Аккермана
+*/
+
+int M = new Random().Next(1,4); 
+int N = new Random().Next(1,4); 
+Console.WriteLine($"Функция Аккермана для чисел {M} и {N} = {AckermannF(M, N)}");
+
+int AckermannF (int M, int N)
+{
+    if (M == 0) return N + 1;
+    if (M != 0 && N == 0) return AckermannF(M - 1, 1);
+    if (M > 0 && N > 0) return AckermannF(M - 1, AckermannF(M, N - 1));
+return AckermannF(M, N);
+}
